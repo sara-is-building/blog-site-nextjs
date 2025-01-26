bc685674ea0ad05fc70dc398aa873dc74683c152
sara-is-building <6403668+sara-is-building@users.noreply.github.com>
2025-01-26 13:16:14 -0800

    Add top navigation

---
+++ app/components/navigation/navigation.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+
+export default function NavBar() {
+    return (
+        <nav className="bg-white border-gray-200 dark:bg-gray-900 inset-x-0 top-0 visible">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="https://lucidtechhub.com" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Lucid Tech Hub</span>
+                </a>
+                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                            <Link key="Home" href="/" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
+                        </li>
+                        <li>
+                            <Link href="/blog" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</Link>
+                        </li>
+                        <li>
+                            <Link href="/book-recommendations" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Book Recommendations</Link>
+                        </li>
+                        <li>
+                            <Link href="/author" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Author</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+};
